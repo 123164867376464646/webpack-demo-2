04afe801a30c28f54e0e83754d8feb2d325f2cb4
linmumu <1054742101@qq.com>
2022-07-10 21:54:27 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
     entry: './src/index.js',
     output: {
         filename: 'index.[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'adc',
+        template: './src/assets/index.html',
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
     },
 };
